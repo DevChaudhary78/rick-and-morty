b363ac1281bef21d0cefbd1468e3dd082dadb138
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-09-24 19:01:56 +0530

    initialized the project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <RickAndMorty></RickAndMorty>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RickAndMorty from './components/RickAndMorty.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RickAndMorty
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #000;
+  color: #fff;
+  padding: 0;
+  margin: 0;
 }
+
 </style>
---
+++ src/components/RickAndMorty.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: "RickAndMorty"
+}
+</script>
+
+<style scoped>
+main {
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+  position: relative;
+}
+</style>
