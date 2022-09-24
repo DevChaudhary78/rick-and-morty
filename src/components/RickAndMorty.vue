e425ea230d9a1ba7fc15e34ef00089c31b7e276c
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-09-24 19:28:06 +0530

    added and placed the important images

--- src/components/RickAndMorty.vue
+++ src/components/RickAndMorty.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
-
+    <img src="../assets/morty.png" alt="The image of rick and morty" id="anchor">
+    <div id="eyes">
+      <img src="../assets/eyes.jpg" alt="Just image of eyes" id="eye" style="top: -200px; left: -72px">
+      <img src="../assets/eyes.jpg" alt="Just image of eyes" id="eye" style="top: -186px; left: -134px">
+      <img src="../assets/eyes.jpg" alt="Just image of eyes" id="eye" style="top: -318px; left: 7px">
+      <img src="../assets/eyes.jpg" alt="Just image of eyes" id="eye" style="top: -317px; left: 59px">
+    </div>
   </main>
 </template>
 
@@ -16,5 +22,14 @@ main {
   place-items: center;
   min-height: 100vh;
   position: relative;
+}
+#eyes, img {
+  position: absolute;
+}
+
+#eye {
+  position: absolute;
+  height: 25px;
+  width: 25px;
 }
 </style>
