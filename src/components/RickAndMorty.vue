e18a945805868697a3d4d0ea6a09051d0decaabb
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-09-25 09:08:50 +0530

    fixed the issue, where eyes are not moving with cursor

--- src/components/RickAndMorty.vue
+++ src/components/RickAndMorty.vue
@@ -12,31 +12,31 @@
 
 <script>
 
-// const angle = function(cx, cy, ex, ey) {
-//   const dy = ey - cy;
-//   const dx = ex - cx;
-//
-//   const rad = Math.atan2(dy, dx);
-//   return rad * 180 / Math.PI;
-// }
-// document.addEventListener("mouseover", (e) => {
-//   const mouseX = e.clientX;
-//   const mouseY = e.clientY;
-//
-//   const anchor = document.getElementById('anchor');
-//   const rekt = anchor.getBoundingClientRect();
-//   const anchorX = rekt.left + rekt.width / 2;
-//   const anchorY = rekt.top + rekt.height / 2;
-//
-//   const eyes = document.querySelectorAll('.eyes');
-//
-//   const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
-//
-//   console.log(angleDeg);
-//   eyes.forEach(eye => {
-//     eye.style.transform = `rotate(${90+angleDeg}deg)`;
-//   })
-// })
+const angle = function(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
+
+  const rad = Math.atan2(dy, dx);
+  return rad * 180 / Math.PI;
+}
+document.addEventListener("mouseover", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const anchor = document.getElementById('anchor');
+  const rekt = anchor.getBoundingClientRect();
+  const anchorX = rekt.left + rekt.width / 2;
+  const anchorY = rekt.top + rekt.height / 2;
+
+  const eyes = document.querySelectorAll('#eye');
+
+  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+  console.log(angleDeg);
+  eyes.forEach(eye => {
+    eye.style.transform = `rotate(${90+angleDeg}deg)`;
+  })
+})
 
 export default {
   name: "RickAndMorty",
