4f846c5a5b369dad254eb1930f3e69285025a397
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-09-25 19:26:13 +0530

    refactored the code according to vuejs style

--- src/components/RickAndMorty.vue
+++ src/components/RickAndMorty.vue
@@ -11,35 +11,69 @@
 </template>
 
 <script>
-
-const angle = function(cx, cy, ex, ey) {
-  const dy = ey - cy;
-  const dx = ex - cx;
-
-  const rad = Math.atan2(dy, dx);
-  return rad * 180 / Math.PI;
-}
-document.addEventListener("mouseover", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-
-  const anchor = document.getElementById('anchor');
-  const rekt = anchor.getBoundingClientRect();
-  const anchorX = rekt.left + rekt.width / 2;
-  const anchorY = rekt.top + rekt.height / 2;
-
-  const eyes = document.querySelectorAll('#eye');
-
-  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
-
-  console.log(angleDeg);
-  eyes.forEach(eye => {
-    eye.style.transform = `rotate(${90+angleDeg}deg)`;
-  })
-})
+//
+//const angle = function(cx, cy, ex, ey) {
+//  const dy = ey - cy;
+//  const dx = ex - cx;
+//
+//  const rad = Math.atan2(dy, dx);
+//  return rad * 180 / Math.PI;
+//}
+//document.addEventListener("mouseover", (e) => {
+//  const mouseX = e.clientX;
+//  const mouseY = e.clientY;
+//
+//  const anchor = document.getElementById('anchor');
+//  const rekt = anchor.getBoundingClientRect();
+//  const anchorX = rekt.left + rekt.width / 2;
+//  const anchorY = rekt.top + rekt.height / 2;
+//
+//  const eyes = document.querySelectorAll('#eye');
+//
+//  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+//
+//  console.log(angleDeg);
+//  eyes.forEach(eye => {
+//    eye.style.transform = `rotate(${90+angleDeg}deg)`;
+//  })
+//})
 
 export default {
   name: "RickAndMorty",
+  methods: {
+    initialize() {
+      document.addEventListener("mouseover", (e) => {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
+
+        this.angleDeg = this.angle(this.mouseX, this.mouseY, this.anchorX, this.anchorY);
+
+        console.log(this.angleDeg);
+
+        this.eyes.forEach(eye => {
+          eye.style.tranform = `rotate(${90+this.angleDeg}deg)`;
+        })
+      })
+    },
+
+    angle(cx, cy, ex, ey) {
+      const dy = ey - cy;
+      const dx = ex - cx;
+
+      const rad = Math.atan2(dy, dx);
+      return rad * 180 / Math.PI;
+    }
+  },
+  mounted() {
+    this.anchor= document.getElementById('anchor');
+    this.rekt= this.anchor.getBoundingClientRect();
+    this.anchorX = this.rekt.left + this.rekt.width / 2;
+    this.anchorY = this.rekt.top + this.rekt.height / 2;
+
+    this.eyes = document.querySelectorAll('#eye')
+
+    this.initialize();
+  }
 }
 </script>
 
